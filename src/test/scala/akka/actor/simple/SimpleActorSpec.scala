91f171d7e9f1400db6f0279b5723c623e6d88ebe
Arek Burdach <arek.burdach@gmail.com>
2015-01-21 22:53:03 +0100

    Test for future.map without ExecutionContext specified

--- src/test/scala/akka/actor/simple/SimpleActorSpec.scala
+++ src/test/scala/akka/actor/simple/SimpleActorSpec.scala
@@ -1,7 +1,10 @@
 package akka.actor.simple
 
 import akka.actor.Actor.Receive
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 
 class SimpleActorSpec extends FlatSpec with Matchers {
 
@@ -12,6 +15,12 @@ class SimpleActorSpec extends FlatSpec with Matchers {
       }
     }
     (actor !? "GO") shouldEqual "OK"
+  }
+
+  it should "map future without ExecutionContext defined" in {
+    val initialFuture = Future(1)
+    val mappedFuture = initialFuture.map(_ + 1)
+    Await.result(mappedFuture, 5.seconds) shouldBe 2
   }
 
 }
